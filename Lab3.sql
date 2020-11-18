5c426d65febf32faaeb19be55c90d29d46c73d13
Joshua <30993319+Joshua-18@users.noreply.github.com>
2020-11-18 17:57:50 -0600

    completed procedure for problem 1

---
+++ Lab3.sql
@@ -0,0 +1,17 @@
+-- Joshua Membreno
+-- #1
+create or replace 
+PROCEDURE FIND_EMPL_DAT
+(p_empnum IN emp.empno%TYPE)
+AS
+rec_empdat emp%ROWTYPE;
+BEGIN
+  SELECT *
+  INTO rec_empdat
+  FROM emp
+  WHERE empno = p_empnum;
+  
+  EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('Employee does not exist!');
+END FIND_EMPL_DAT;
